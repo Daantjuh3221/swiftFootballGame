e3a71da5c52caf80d734999d0411af05c83b5940
Daan Befort <daanbefort@c1mk86xxdty3.home>
2017-03-11 13:03:35 +0100

    Initial Commit

---
+++ swiftControllerFootballGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftControllerFootballGame
+//
+//  Created by Daan Befort on 11/03/2017.
+//  Copyright © 2017 Daan Befort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
